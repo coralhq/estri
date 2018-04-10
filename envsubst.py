06a1cb09f40027b6fc442b1f9b1c1d565370d298
Akhyar Amarullah <akhyar.amarullah@midtrans.com>
2018-04-10 10:11:14 +0700

    Make dns resolver and S3 provider customizable

--- envsubst.py
+++ envsubst.py
@@ -3,5 +3,8 @@
 import os
 import sys
 
+os.environ.setdefault('DNS_RESOLVER', '1.1.1.1')
+os.environ.setdefault('S3_DOMAIN', 's3.amazonaws.com')
+
 with open('default.conf') as f:
     sys.stdout.write(os.path.expandvars(f.read()))
